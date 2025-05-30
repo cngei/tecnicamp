e3e2d79307cbe9db9e7e4bb6c476a972eb6edef6
Paolo Campanelli <paolo.campanelli@cngei.it>
2025-05-30 22:16:02 +0200

    FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Search } from '@mui/icons-material';
-import { AppBar, Autocomplete, Box, Card, CardContent, CardMedia, Chip, Container, InputAdornment, Slider, TextField, Toolbar, Typography, useMediaQuery } from '@mui/material';
+import { ExpandMore, Info, Search } from '@mui/icons-material';
+import { AccordionDetails, Accordion, AccordionSummary, Alert, AppBar, Autocomplete, Box, Card, CardContent, CardMedia, Chip, Container, InputAdornment, Slider, TextField, Toolbar, Typography, useMediaQuery, List, ListItem, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import data from './assets/data.json';
 
@@ -107,13 +107,86 @@ function App() {
       </AppBar>
       <Box display="flex" flexDirection="column" gap={2} width="100%" mx={0}>
           <Container sx={{py: 2}}>
-            <Typography variant="body1">
+            <Alert icon={<Info fontSize="inherit" />} severity="info">
+              Le iscrizioni apriranno <strong>domenica 1 giugno alle 12:00</strong> e resteranno aperte fino al <strong>30 giugno</strong>.
+            </Alert>
+            <Typography variant="body1" sx={{my: 2}}>
               Il <strong>Tecnicamp</strong> è un’Attività Nazionale della Branca <strong>Esploratori ed Esploratrici</strong>: sviluppato in più basi scout in tutta Italia, permette ad esploratrici ed esploratori di migliorare la propria conoscenza delle tecniche scout e di sperimentarne di nuove.
               <br />
               Edizione 2025: <strong>6 basi</strong> Tecnicamp lungo tutto il territorio italiano, sempre più strutturate nelle proposte e nelle tecniche per cercare di arrivare a tutti e tutte. Inoltre da quest’anno, le nostre basi saranno caratterizzate da un nome che sia per loro unico ed evocativo, con un pizzico di tecnica ma anche di sogni, aiutandoci a dare ulteriore <strong>slancio</strong> a questo fantastico evento nazionale: soffiano i <strong>Venti</strong> dell’avventura!
               <br />
               Da quale vi lascerete rapire?
             </Typography>
+            <Box sx={{display: 'flex', flexDirection: 'row', gap: 2, my: 2, justifyContent: 'center'}}>
+              <Button variant="contained" color="primary" href="https://app.cngei.it">
+                Accedi al portale
+              </Button>
+              <Button variant="outlined" href="https://help.cngei.it/servicedesk/customer/portal/4">
+                Richieste di assistenza
+              </Button>
+            </Box>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography component="span">Come posso pagare la quota di iscrizione?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                Hai 3 possibilità:
+                <List>
+                  <ListItem>1. Bonifico cumulativo da parte della tua sezione</ListItem>
+                  <ListItem>2. Pagare tramite bonifico bancario</ListItem>
+                  <ListItem>3. Pagare tramite PayPal</ListItem>
+                </List>
+                L'iscrizione è confermata solo dopo il pagamento, consigliamo di pagare il più presto possibile per evitare che terminino i posti per il corso scelto.
+                <Typography variant="body2">
+                  INTESTAZIONE: CNGEI<br/>
+                  IBAN: IT 23 Z 06230 03205 000040421938<br/>
+                  CAUSALE BONIFICO: “Tecnicamp 2025 - Sezione – Nome e Cognome esploratore” (es. Tecnicamp 2025 - Aosta – Mario Rossi)
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography component="span"> Non riesco ad accedere al portale. Non mi arriva nessuna email per l’accesso. Che cosa devo fare? </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+              Contatta subito il tuo Capo Reparto e chiedigli di verificare se la mail indicata nella tua anagrafica presente app.cngei.it è corretta, facendo attenzione ad eventuali spazi bianchi o punti inseriti nel campo.
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography component="span">Il corso che volevo fare è già completo. Posso mettermi in lista di attesa?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+              La lista di attesa non esiste. Non scoraggiarti, non ti resta che iscriverti ad un altro corso Tecnicamp. Siamo sicuri che saprai scegliere una valida alternativa tra le numerose proposte. Puoi comunque indicare nel modulo il corso “desiderato” che risulta però completo; se dovesse liberarsi un posto, terremo conto del tuo desiderio.
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography component="span">Devo modificare il contenuto della mia iscrizione. Cosa faccio?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+              Non è possibile modificare il contenuto in autonomia. Apri un ticket di assistenza spiegando le modifiche da fare.
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography component="span">Ho cambiato idea e voglio cambiare la scelta del corso</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+              Se vuoi cambiare corso, prova a scriverci la tua nuova scelta. Non assicuriamo di riuscire ad accontentarti, ma proveremo a vedere se ci sono ancora posti liberi nel nuovo corso e ti daremo una risposta.
+  Se invece vuoi scambiare il corso con un altro esplo del tuo reparto (e siete entrambə d’accordo), chiedi al tuo Capo Reparto di aprire un ticket per richiedere lo scambio.
+              </AccordionDetails>
+            </Accordion>
           </Container>
           {filteredData.map(base => (
             <Box key={base.nome} sx={{display: 'flex', flexDirection: 'column', gap: 2, justifyContent: 'center', alignItems: 'center'}}>
